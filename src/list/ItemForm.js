9514adbed8ca291553505c7222235adcb1b30f1c
Benjamin Hansen <bhans23@gmail.com>
2019-12-17 13:47:18 -0700

    Added some styling

--- src/list/ItemForm.js
+++ src/list/ItemForm.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 
 class ItemForm extends Component {
@@ -11,22 +13,23 @@ class ItemForm extends Component {
                       // todoValue: 'user input'
     this.setState({ [name]: value  })
   }
-
+  
   handleSubmit = (e) => {
+    console.log('here')
     e.preventDefault() // stop reloading, that loses user data
     // create crud action
     this.props.addItem(this.state)
     // rest form 
-    this.setState({ name: '' })
+    this.setState({ name: '', price: '' })
   }
 
   render() {
     return(
       <div>
        <form onSubmit={this.handleSubmit}>
-        <input value={this.state.name} name='name' onChange={this.handleChange} required placeholder='Add an item'/>
-       <input value={this.state.price} name='price' onChange={this.handleChange} required placeholder='Price' />
-       <button>Add Item</button>
+        <TextField value={this.state.name} name='name' onChange={this.handleChange} required placeholder='Add an item' style={{marginRight:8}}/>
+       <TextField value={this.state.price} name="price" onChange={this.handleChange} required placeholder="Price" type="number" />
+       <Button type="submit" variant="contained" color="primary"  style={{marginLeft:8}}>Add Item</Button>
        </form>
     </div>
     )
